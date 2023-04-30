fefda70eb560c1dddb83792b6d4ec512bd151a1c
home <jasuil@daum.net>
2023-04-30 23:46:43 +0900

    test chat

---
+++ service/chat.py
@@ -0,0 +1,13 @@
+import asyncio
+from websockets.server import serve
+
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+async def main():
+    async with serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
